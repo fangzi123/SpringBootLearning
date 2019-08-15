3d7e15b762331706dbfa161d54b1367d282828ee
wangff <1024904798@qq.com>
2019-08-15 18:17:07 +0800

    get

--- sharding-jdbc-example/sharding-jdbc-master-slave/src/main/java/com/forezp/shardingjdbcmasterslave/service/impl/UserServiceImpl.java
+++ sharding-jdbc-example/sharding-jdbc-master-slave/src/main/java/com/forezp/shardingjdbcmasterslave/service/impl/UserServiceImpl.java
@@ -1,11 +1,9 @@
 package com.forezp.shardingjdbcmasterslave.service.impl;
 
-import com.forezp.shardingjdbcmasterslave.repository.UserRepository;
 import com.forezp.shardingjdbcmasterslave.entity.User;
+import com.forezp.shardingjdbcmasterslave.repository.UserRepository;
 import com.forezp.shardingjdbcmasterslave.service.UserService;
-
 import io.shardingsphere.api.HintManager;
-import org.mybatis.spring.support.SqlSessionDaoSupport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -27,7 +25,6 @@ public class UserServiceImpl  implements UserService {
 
     @Override
     public List<User> list() {
-
         return userRepository.list();
     }
 }
